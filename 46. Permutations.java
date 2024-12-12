b4bd230aadf7dc5ee787859995d0d15d154bcd37
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2024-12-12 13:54:10 +0530

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -1,4 +1,4 @@
-
+// approach - 1
 class Solution {
     private void recurPermute(List<Integer> ds, int[] nums, List<List<Integer>> ans, int[] freq) {
         if (ds.size() == nums.length) {
@@ -23,5 +23,35 @@ class Solution {
         for (int i = 0; i < nums.length; i++) freq[i] = 0;
         recurPermute(ds, nums, ans, freq);
         return ans;
+    }
+}
+
+// approach - 2
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        check(0,nums,ans);
+        return ans;
+    }
+    private void check(int ind, int[] nums,List<List<Integer>> ans){
+        if ( ind == nums.length){
+            List < Integer > ds = new ArrayList < > ();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+        for ( int i = ind; i < nums.length; i++){
+            swap(i,ind,nums);
+            check(ind+1,nums,ans);
+            swap(i,ind,nums);  
+        }
+    }
+    public void swap(int i , int ind,int[] nums){
+            int temp = nums[i];
+            nums[i] = nums[ind];
+            nums[ind] = temp;
     }
 }
