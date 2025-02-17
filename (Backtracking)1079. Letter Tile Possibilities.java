b0e58c1ff722e89a60f938d6ab471292442a285b
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-02-17 17:57:12 +0530

    Update (Backtracking)1079. Letter Tile Possibilities.java

--- (Backtracking)1079. Letter Tile Possibilities.java
+++ (Backtracking)1079. Letter Tile Possibilities.java
@@ -20,3 +20,32 @@ class Solution {
         }
     }
 }
+
+// since we don't need to find string we just need count we can maintian freq array to solve
+
+class Solution {
+    int n;
+    public int numTilePossibilities(String tiles) {
+        n = tiles.length();
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++){
+            freq[tiles.charAt(i)-'A']++;
+        }
+        int count = backtrack(freq);
+        return count;
+    }
+    public int backtrack(int[] freq){
+        int count = 0;
+        for ( int i = 0; i < 26; i++){
+            if ( freq[i] == 0){
+                continue;
+            }
+            count++;
+            freq[i]--;
+            count += backtrack(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+}
+
